b1c737ce96dd84332516c74e7789e9294971befc
Lukeveille <luke@lukeleveille.com>
2018-09-08 13:14:11 -0400

    Completed research, checked that all statements ran without errors

--- research_exercise.rb
+++ research_exercise.rb
@@ -2,8 +2,8 @@
 # .length
 # Does not accept any arguments
 # Returns an int that represents the quantity of descrete values within a variable, aka. "environment variables".
-"A string".length # == 8
 [1, 2, 3, 4].length # == 4
+"A string".length # == 8
 
 # .strip
 # Does not accept any arguments, will remove any blank space/new line char at the beginning and/or end of a string it is called on.
@@ -19,8 +19,8 @@
 # .start_with?
 # Accepts 1 or more strings as an argument, and returns true if said string can be found at the start of the string the method was applied to.
 # Returns a boolean true or false.
-"hello".starts_with?("hell") # == true
-"hello".starts_with?("lo") # == false
+"hello".start_with?("hell") # == true
+"hello".start_with?("lo") # == false
 
 # ARRAYS
 # .first
@@ -49,20 +49,35 @@
 
 # HASHES
 # .to_a
-
+# Converts a hash into an array of nested arrays containing each key, value pairing. Does not accept any arguments.
+# Returns an array of nested arrays.
+{:breakfast=>"cereal", :lunch=>"sandwich", :dinner=>"roti"}.to_a # == [[:breakfast, "cereal"], [:lunch, "sandwich"], [:dinner, "roti"]]
 
 # has_key?
-
+# Checks if a hash contains a specific key. Requires one argument, the name of a key.
+# Returns a boolean value.
+{:breakfast=>"cereal", :lunch=>"sandwich", :dinner=>"roti"}.has_key?(:lunch) # == true
+{:breakfast=>"cereal", :lunch=>"sandwich", :dinner=>"roti"}.has_key?(:snack) # == false
 
 # has_value?
-
+# Checks if a hash contains a specific value. Requires one argument, a value.
+# Returns a boolean value.
+{:breakfast=>"cereal", :lunch=>"sandwich", :dinner=>"roti"}.has_key?("cereal") # == true
+{:breakfast=>"cereal", :lunch=>"sandwich", :dinner=>"roti"}.has_key?("apple") # == false
 
 # TIME
 # .now
-
+# Will return the current GMT time and date, in seconds.
+Time.now # == 2018-09-08 16:33:29 +0000
 
 # FILE
 # exist?
+# Checks if a file exists. Accepts a file object as the argument.
+# Returns a boolean value.
+File.exist?("not_a_file.txt") # == false
+File.exist?("file.txt") # == True
 
-
-#extname
+# extname
+# Will extract the extension of a file object.
+# Returns a string of the file extension.
+File.exist?("file.txt") # == ".txt"
